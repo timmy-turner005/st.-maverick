5162ac91ee14e47fbf98b258f07818f5b18cd0e4
timmy-turner005 <timitalented005@gmail.com>
2025-11-12 17:10:28 +0100

    first commit

--- app/page.js
+++ app/page.js
@@ -1,66 +1,61 @@
+import classes from "./page.module.css";
+import { Fragment } from "react";
 import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
+export default function LoginPage() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <Fragment>
+      <header className={classes.header}>
+        <h2 className={classes.title}>
+          Welcome to <span>st. maverick schools</span>
+        </h2>
+      </header>
+      <div className={classes.subtitles}>
+        <p className={classes.subtitle}>Maverick Schools Portal</p>
+      </div>
+      <section className={classes.container}>
+        <div className={classes.image}>
+          <Image
+            src="/logo.png"
+            alt="mavrerick schools logo"
+            width={227}
+            height={279}
+            className={classes.logo}
+          />
+          <figcaption className={classes.logos}>
+            ST. MAVERICK SCHOOLS
+          </figcaption>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className={classes["form-container"]}>
+          <span>LOG IN</span>
+          <form className={classes.form}>
+            <div className={classes["input-group"]}>
+              <label htmlFor="name"></label>
+              <input type="name" id="name" placeholder="Name" required />
+            </div>
+            <div className={classes["input-group"]}>
+              <label htmlFor="password"></label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="remember">Remember Me</label>
+              <input type="checkbox" id="checkbox" required />
+              <a href="#" className={classes.forgotPassword}>
+                Forgot Password?
+              </a>
+            </div>
+            <button type="submit" className={classes.button}>
+              Log In
+            </button>
+          </form>
         </div>
-      </main>
-    </div>
+      </section>
+    </Fragment>
   );
 }
